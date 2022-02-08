663cb03356b48865ec00802cfbdcff97d2195aae
Blackbamb <88196452+Blackbamb@users.noreply.github.com>
2022-02-08 12:13:47 +0300

    Fix (refactor code)

--- Ex_2/computer_move.py
+++ Ex_2/computer_move.py
@@ -10,7 +10,7 @@ def battle(mas, sign):
             for j in range(len):
                 if mas[i][j] == 'x':
                     count += 1
-                    if count >= lvl and (j + 1) <len and mas[i][j + 1] == 0:
+                    if count >= lvl and (j + 1) < len and mas[i][j + 1] == 0:
                         mas[i][j + 1] = sign
                         return mas[i][j + 1]
                 else:
@@ -158,9 +158,152 @@ def computer_win(mas, sign, signx):
     global count
     for i in range(len):
         for j in range(len):
+            if mas[i][j] == sign:
+                count += 1
+                if count == 4 and (j + 1) < len and mas[i][j + 1] == 0:
+                    mas[i][j + 1] = sign
+                    return mas[i][j + 1]
+            else:
+                count = 0
+
+    for i in range(len):
+        for j in range(len):
+            if mas[i][j] == sign:
+                count += 1
+                if count == 4 and (j - count) >= 0 and mas[i][j - count] == 0:
+                    mas[i][j - count] = sign
+                    return mas[i][j - count]
+            else:
+                count = 0
+
+    for i in range(len):
+        for j in range(len):
+            if mas[j][i] == sign:
+                count += 1
+                if count == 4 and (j + 1) < len and mas[j + 1][i] == 0:
+                    mas[j + 1][i] = sign
+                    return mas[j][i + 1]
+            else:
+                count = 0
+
+    for i in range(len):
+        for j in range(len):
+            if mas[j][i] == sign:
+                count += 1
+                if count == 4 and (j - count) >= 0 and mas[j - count][i] == 0:
+                    mas[j - count][i] = sign
+                    return mas[j - count][i]
+            else:
+                count = 0
+
+    for i in range(len):  # низ главной диагонали
+        for j in range(len - i):
+            if mas[j + i][j] == sign:
+                count += 1
+                if count == 4 and (j + i + 1) < len and (j + 1) < len and mas[j + i + 1][j + 1] == 0:
+                    mas[j + i + 1][j + 1] = sign
+                    return mas[j + i + 1][j + 1]
+            else:
+                count = 0
+
+    for i in range(len):  # низ главной диагонали
+        for j in range(len - i):
+            if mas[j + i][j] == sign:
+                count += 1
+                if count == 4 and (j + i - count) >= 0 and (j - count) >= 0 and mas[j + i - count][j - count] == 0:
+                    mas[j + i - count][j - count] = sign
+                    return mas[j + i - count][j - count]
+            else:
+                count = 0
+
+    for i in range(len):  # сверху главной диагонали
+        for j in range(len - i):
+            if mas[j][i + j] == sign:
+                count += 1
+                if count == 4 and (j + i + 1) < len and (j + 1) < len and mas[j + 1][j + i + 1] == 0:
+                    mas[j + 1][j + i + 1] = sign
+                    return mas[j + 1][j + i + 1]
+            else:
+                count = 0
+
+    for i in range(len):  # сверху главной диагонали
+        for j in range(len - i):
+            if mas[j][i + j] == sign:
+                count += 1
+                if count == 4 and (j + i - count) >= 0 and (j - count) >= 0 and mas[j - count][j + i - count] == 0:
+                    mas[j - count][j + i - count] = sign
+                    return mas[j - count][j + i - count]
+            else:
+                count = 0
+
+    for n in range(len - 1, 8, -1):
+        count = 0  # побочная диагональ
+        for i in range(len):
+            if mas[i][n - i] == sign:
+                count += 1
+                if count == 4 and (i + 1) < len and (n - i - 1) < len and mas[i + 1][n - i - 1] == 0:
+                    mas[i + 1][n - i - 1] = sign
+                    return mas[i + 1][n - i - 1]
+            else:
+                count = 0
+
+    for n in range(len - 1, 8, -1):
+        count = 0  # побочная диагональ
+        for i in range(len):
+            if mas[i][n - i] == sign:
+                count += 1
+                if count == 4 and (i - count) >= 0 and (n - i + count) < len and mas[i - count][n - i + count] == 0:
+                    mas[i - count][n - i + count] = sign
+                    return mas[i - count][n - i + count]
+            else:
+                count = 0
+
+    for n in range(1, len, 1):  # побочная диагональ снизу
+        for i in range(len - n):
+            if mas[i + n][len - 1 - i] == sign:
+                count += 1
+                if count == 4 and (i + n + 1) < len and (len - 2 - i) < len and mas[i + n + 1][len - 2 - i] == 0:
+                    mas[i + n + 1][len - 2 - i] = sign
+                    return mas[i + n + 1][len - 2 - i]
+            else:
+                count = 0
+
+    for n in range(1, len, 1):  # побочная диагональ снизу
+        for i in range(len - n):
+            if mas[i + n][len - 1 - i] == sign:
+                count += 1
+                if count == 4 and (i + n - count) >= 0 and (len - 1 - i + count) < len and mas[i + n - count][
+                    len - 1 - i + count] == 0:
+                    mas[i + n - count][len - 1 - i + count] = sign
+                    return mas[i + n - count][len - 1 - i + count]
+            else:
+                count = 0
+
+    for n in range(len - 1, 1, -1):  # побочная диагональ сверху
+        for i in range(n):
+            if mas[i][n - 1 - i] == sign:
+                count += 1
+                if count == 4 and (i + 1) >= 0 and (n - 2 - i) >= 0 and mas[i + 1][n - 2 - i] == 0:
+                    mas[i + 1][n - 2 - i] = sign
+                    return mas[i + 1][n - 2 - i]
+            else:
+                count = 0
+
+    for n in range(len - 1, 1, -1):  # побочная диагональ сверху
+        for i in range(n):
+            if mas[i][n - 1 - i] == sign:
+                count += 1
+                if count == 4 and (i - count) >= 0 and (n - 1 - i + count) < (len - 1) and mas[i - count][
+                    n - 1 - i + count] == 0:
+                    mas[i - count][n - 1 - i + count] = sign
+                    return mas[i - count][n - 1 - i + count]
+            else:
+                count = 0
+    for i in range(len):
+        for j in range(len):
             if mas[i][j] == signx:
                 count += 1
-                if count == kol and (j + 1) >=0 and mas[i][j + 1] == 0:
+                if count == kol and (j + 1) >= 0 and mas[i][j + 1] == 0:
                     mas[i][j + 1] = 'o'
                     return mas[i][j + 1]
             else:
